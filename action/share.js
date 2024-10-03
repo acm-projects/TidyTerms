c60bc9a98b5282d73bddc3ebdd65099f47489f53
Vivian-Nguyen-08 <143738359+Vivian-Nguyen-08@users.noreply.github.com>
2024-10-03 12:16:19 -0500

    Fixed the share page and image overlay

--- action/share.js
+++
@@ -1,5 +0,0 @@
-// function shareToGmail() {
-//     window.location.href = 'https://mail.google.com/mail/?view=cm&fs=1&to=&su=Terms%20and%20Conditions%20Summary&body=This%20is%20the%20email%20body.';
-// }
-// //Format for empty: &to=&su=&body=
-// ///will need to in the body have a copy and paste version in the gmail of what we copied forom summary
